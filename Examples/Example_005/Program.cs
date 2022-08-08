3d65a6a1742acc08871ee0962e35d8eff9ca14e1
Dima Tarasov <desant74268@yandex.ru>
2022-08-08 09:34:32 +0300

    if/else

---
+++ Examples/Example_005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+
+if (username.ToLower() == "маша"){
+    Console.WriteLine("Урааа, это Маша");
+} else {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
