7d2d69dced8c2b86a3c99a563a49765acb8b7665
Dima Tarasov <desant74268@yandex.ru>
2022-08-09 11:57:00 +0300

    add IntroMethod

---
+++ Examples/Example_008_IntroMetod/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a = 69;
+int b = 44;
+int c = 8;
+
+int max = Max(a, b, c);
+
+Console.WriteLine(max);
