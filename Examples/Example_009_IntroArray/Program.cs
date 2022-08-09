a7b5fa2e88d2ff8f4408b34a875076f0e6905cc9
Dima Tarasov <desant74268@yandex.ru>
2022-08-09 11:58:23 +0300

    add IntroArray

---
+++ Examples/Example_009_IntroArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 21, 31 };
+
+int max = Max(array[0], array[1], array[2]);
+
+Console.WriteLine(max);
